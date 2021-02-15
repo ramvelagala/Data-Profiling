2dfbfbc93f09e84113b68ce948b9bfefbe1378b4
Ram <kittuvelagala@gmail.com>
2021-02-15 14:36:51 -0600

    Added printing 20 the file

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(5)
+    print(20)  # Added printing 20
 
 
 # Press the green button in the gutter to run the script.
